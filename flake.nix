dd18ca57cad5f87c1abd78a4954d85a9856b1de8
Balthild <ibalthild@gmail.com>
2025-05-22 15:14:33 -0500

    Init

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { pkgs, ... }: {
+        devShells.default = with pkgs; mkShell {
+          nativeBuildInputs = [ cmake ];
+          buildInputs = [ libiconv ];
+        };
+      };
+    };
+}
